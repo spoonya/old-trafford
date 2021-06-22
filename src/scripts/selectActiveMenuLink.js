da5aa46114305475dfdbc17ad8d09a12486fb495
spoonya <nomad8t@gmail.com>
2021-06-22 19:59:41 +0300

    fix: cost list

--- src/scripts/selectActiveMenuLink.js
+++ src/scripts/selectActiveMenuLink.js
@@ -1,16 +1,51 @@
 export default function selectActiveMenuLink() {
   const curLocation = window.location.href;
   const origin = `${window.location.origin}/`;
-  const menuLinks = document.querySelectorAll('.header__menu a');
+  const pathName = window.location.pathname;
+  const { hash } = window.location;
+  const menuLinks = [...document.querySelectorAll('.header__menu a')];
 
-  for (let i = 0; i < menuLinks.length; i++) {
-    if (curLocation === origin) {
-      menuLinks[0].classList.add('active');
-      return;
+  const indexPathName = '/index.html';
+  const serviceHrefs = [
+    '/service-onetime.html',
+    '/service-complex.html',
+    '/service-repair.html',
+    '/service-law.html'
+  ];
+
+  const findLink = (comparator) =>
+    menuLinks.find((el) => el.textContent === comparator);
+
+  const setActive = (linkEl) => {
+    linkEl.classList.add('active');
+  };
+
+  const isIndexPage = () => {
+    if (curLocation === origin || pathName === indexPathName) {
+      setActive(findLink('Главная'));
+
+      return true;
     }
 
-    if (menuLinks[i].href === curLocation) {
-      menuLinks[i].classList.add('active');
+    return false;
+  };
+
+  const isServicePage = () => {
+    if (serviceHrefs.includes(pathName)) {
+      setActive(findLink('Услуги'));
+
+      return true;
+    }
+
+    return false;
+  };
+
+  for (let i = 0; i < menuLinks.length; i++) {
+    if (isIndexPage()) return;
+    if (isServicePage()) return;
+
+    if (!hash && menuLinks[i].href === curLocation) {
+      setActive(menuLinks[i]);
     }
   }
 }
