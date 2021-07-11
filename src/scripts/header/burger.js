b19d1bbfde3b8e8b02b64542367bbadd0cb65199
spoonya <nomad8t@gmail.com>
2021-07-11 14:06:58 +0300

    refactor: burger animation

--- src/scripts/header/burger.js
+++ src/scripts/header/burger.js
@@ -1,12 +1,6 @@
 import { CLASSES, DOM_ELEMENTS } from '../constants';
 import { isMediaBreakpoint } from '../helpers';
 
-const animationConfig = {
-  duration: 150,
-  easing: 'ease-in',
-  iterations: 1
-};
-
 function isBurgerOpened() {
   if (!DOM_ELEMENTS.burger.classList.contains(CLASSES.active)) {
     return false;
@@ -15,37 +9,8 @@ function isBurgerOpened() {
   return true;
 }
 
-function activateAnimation(onClose = false) {
-  const selectAnimeDir = () => {
-    if (!onClose) {
-      return [
-        {
-          opacity: '0'
-        },
-        {
-          opacity: '1'
-        }
-      ];
-    }
-    return [
-      {
-        opacity: '1'
-      },
-      {
-        opacity: '0'
-      }
-    ];
-  };
-
-  DOM_ELEMENTS.headerTopWrapper.animate(selectAnimeDir(), {
-    ...animationConfig
-  });
-}
-
 function toggleBurger() {
   DOM_ELEMENTS.burger.addEventListener('click', () => {
-    activateAnimation();
-
     DOM_ELEMENTS.burger.classList.toggle(CLASSES.active);
     DOM_ELEMENTS.headerInfo.classList.add(CLASSES.active);
     DOM_ELEMENTS.headerTopWrapper.classList.toggle(CLASSES.active);
@@ -54,14 +19,10 @@ function toggleBurger() {
 }
 
 function removeBurger() {
-  if (isMediaBreakpoint()) activateAnimation(true);
-
-  setTimeout(() => {
-    DOM_ELEMENTS.burger.classList.remove(CLASSES.active);
-    DOM_ELEMENTS.headerInfo.classList.remove(CLASSES.active);
-    DOM_ELEMENTS.headerTopWrapper.classList.remove(CLASSES.active);
-    DOM_ELEMENTS.body.classList.remove(CLASSES.scrollHidden);
-  }, animationConfig.duration);
+  DOM_ELEMENTS.burger.classList.remove(CLASSES.active);
+  DOM_ELEMENTS.headerInfo.classList.remove(CLASSES.active);
+  DOM_ELEMENTS.headerTopWrapper.classList.remove(CLASSES.active);
+  DOM_ELEMENTS.body.classList.remove(CLASSES.scrollHidden);
 }
 
 function removeBurgerOnMedia() {
