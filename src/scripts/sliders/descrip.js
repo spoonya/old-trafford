15d0bb9039b0138e7cd52cf7ebee7a64c360c160
spoonya <nomad8t@gmail.com>
2021-07-13 21:43:58 +0300

    test

--- src/scripts/sliders/descrip.js
+++ src/scripts/sliders/descrip.js
@@ -12,11 +12,6 @@ const swiperDescrip = new Swiper('#swiper-descrip', {
   observer: true,
   observeParents: true,
 
-  autoplay: {
-    delay: 5000,
-    pauseOnMouseEnter: true
-  },
-
   pagination: {
     el: '.swiper-pagination',
     clickable: true
