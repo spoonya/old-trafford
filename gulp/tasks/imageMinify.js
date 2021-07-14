253fe940cf950682b8403def738075379ee5aa3f
spoonya <nomad8t@gmail.com>
2021-07-14 16:37:32 +0300

    feat: add map placeholder

--- gulp/tasks/imageMinify.js
+++ gulp/tasks/imageMinify.js
@@ -3,7 +3,7 @@ const imagemin = require('gulp-imagemin');
 
 module.exports = function imageMinify() {
   return gulp
-    .src('src/images/**/*.{gif,png,jpg,svg,webp}')
+    .src('src/images/**/*.{gif,png,jpg,jpeg,svg,webp}')
     .pipe(
       imagemin([
         imagemin.gifsicle({ interlaced: true }),
